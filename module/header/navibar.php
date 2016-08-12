a5e8ec3ad4aee001e15c79666336991fd5e58961
Yukito <Yukito@YUKITO-LT>
2016-08-13 02:26:53 +0700

    Revert "Sửa menu và hàm load danh sách tin theo chủ đề con"
    This reverts commit 1d5a77f8628b366e121b89e9bcf6e50e11fc6a7d.

--- module/header/navibar.php
+++ module/header/navibar.php
@@ -4,43 +4,15 @@
             <?php
             require_once getLibsPath('connect');
             require_once getModelPath('model_danhmuc');
-            $data=danh_sach_dm_ex($conn);
-            for($i=0;$i<count($data);$i++)
+            foreach (danh_sach_dm($conn) as $data)
             {
-                if($data[$i]['parent_id'] !=0)
-                    break;
-                for($j=$i+1;$j<count($data);$j++)
-                {
-                    if($data[$i]['id']==$data[$j]['parent_id'])
-                        break;
-                }
             ?>
             <li<?php
-                if(isset($_GET['cat']) && $_GET['cat'] == $data[$i]['id'])
-                {
-                    echo ' class="active',$j<count($data)?' dropdown':'','"';
-                }
-                else
-                    echo $j<count($data)?' class="dropdown"':'';
-               ?>>
-                <a href="<?php echo getMyLink(array('cat'=>$data[$i]['id']));?>"><?php echo $data[$i]['name'];?></a>
+                if(isset($_GET['cat']))
+                    if($_GET['cat'] == $data['id'])
+                        echo ' class="active"';
+               ?>><a href="<?php echo getMyLink(array('cat'=>$data['id']));?>"><?php echo $data['name'];?></a></li>
             <?php
-                if(j<count($data))
-                {
-            ?>
-                <div class="dropdown-content">
-                    <?php
-                        for($j;$j<count($data);$j++)
-                        {
-                            if($data[$i]['id']!=$data[$j]['parent_id'])
-                                break;
-                            echo '<a href="',getMyLink(array('cat'=>$data[$j]['id'])),'">',$data[$j]['name'],'</a>';
-                        }
-                    ?>
-                </div>
-            </li>
-                <?php
-                }
             }
             ?>
         </ul>
