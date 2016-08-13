046b82829b98f2f1f996f08965639c329ae81f62
tap <phongtran270890@gmail.com>
2016-08-13 11:10:08 +0700

    @@

--- padmin/module/dashboard/index.php
+++ padmin/module/dashboard/index.php
@@ -1,7 +1,7 @@
 <table class="function_table" style="margin: 0px auto;">
 	<tr>
 		<td class="function_item user_add"><a href="index.php?p=them-user">Thêm user</a></td>
-		<td class="function_item user_list"><a href="index.php?p=quan-ly-user">Quản lý user</a></td>
+		<td class="function_item user_list"><a href="index.php?p=danh-sach-user">Quản lý user</a></td>
 		<td rowspan="3" class="statistics_panel">
 			<h3>Thống kê:</h3>
 			<ul>
